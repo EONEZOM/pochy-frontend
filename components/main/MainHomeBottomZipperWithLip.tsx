349193892e63bac5214f6a3cddf37e80f49892dd
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-05-15 19:08:11 +0900

    HotFix : 빌드 에러 해결

--- components/main/MainHomeBottomZipperWithLip.tsx
+++ components/main/MainHomeBottomZipperWithLip.tsx
@@ -42,7 +42,6 @@ export function MainHomeBottomZipperWithLip({
           priority
           fetchPriority="high"
         />
-        <div className="pointer-events-none absolute right-2 bottom-[-10px] z-10 w-[min(28vw,112px)] sm:right-3 sm:bottom-3">
         <div className="pointer-events-none absolute right-2 bottom-0 z-10 w-[min(28vw,112px)] sm:right-3 sm:bottom-3">
           <Image
             src="/figma/main/립스틱.svg"
